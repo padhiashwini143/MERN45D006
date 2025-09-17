850a85604b1cb1574a7936c69c46b83bac815e8e
padhiashwini143 <padhiashwini143@gmail.com>
2025-09-17 16:05:34 +0530

    Day1

---
+++ sept/week3/Day1/array.js
@@ -0,0 +1,53 @@
+console.log("Today we are going to learn Array methods")
+
+// const arr=[1,2,3,4,5,6,7,"Qlith",{name:"xyz",age:20},["deepak","chandan","ashwini","bharat","faruk"]];
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+
+// for(let v of arr){
+//     console.log(v)
+// }
+
+// const nameArr=arr[arr.length-1];
+// for let 
+
+// let numArr=[85,97,44,37,76,60];
+
+// let sum=0;
+// for(let v of numArr){
+//     sum=sum+v;
+
+// }
+// console.log("Average of the Element:",sum/(numArr.lengths))
+
+// numArr.push(70,80,"ashwini")
+// console.log(numArr)
+
+// numArr
+
+let strArr=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+
+strArr.shift("Bloomberg")
+console.log(strArr)
+
+strArr.splice(1,1,"Ola")
+console.log(strArr)
+
+strArr.push("Amazon")
+console.log(strArr)
+
+//function
+//function declaration
+// function printSomething(){
+//     console.log("printing")
+// }
+
+// //function calling
+// printSomething()
+
+let printSomething=function(a){
+    console.log("printing",a)
+}
+printSomething(10)
+
