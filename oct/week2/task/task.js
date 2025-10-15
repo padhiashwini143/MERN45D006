0e0cadd74b4be59fa42f3904ba732c074ab4d0da
ashwinikumarpadhi2004-cmd <padhiashwini143@gmail.com>
2025-10-15 15:13:46 +0530

    push all

--- oct/week2/task/task.js
+++ oct/week2/task/task.js
@@ -3,9 +3,27 @@ console.log("this is our server page")
 const express = require("express");
 const app = express()
 
-app.get("/api", (reg,res) =>{
-    console.log("get request called")
+app.use(express.json())
+
+app.get("/api", (req,res) =>{
+    const data=req.body;
+    console.log("get request called",data)
     return res.send("get request called")
+})
+app.post("/post", (req,res) =>{
+    const data=req.body;
+    console.log("post request called",data)
+    return res.send("post request called")
+})
+app.put("/put", (req,res) =>{
+    const data=req.body;
+    console.log("put request called",data)
+    return res.send("put request called")
+})
+app.delete("/api", (req,res) =>{
+    const data=req.body;
+    console.log("delete request called",data)
+    return res.send("delete request called")
 })
 
 const port = 10000;
